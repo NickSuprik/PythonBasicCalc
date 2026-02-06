5a3b89fc4b2271f5dc71ae9d9e6ba04f020a9bfd
unknown <nsuprik@gmail.com>
2026-02-06 12:59:53 -0700

    2nd version of script , both number input working, needs menu and arithmatic logic

--- PythonSimpleCalc.py
+++ PythonSimpleCalc.py
@@ -1,4 +1,23 @@
 #powershell line to run script:
 #python SCRIPTNAMEHERE.py
 
-print("hello py")
+#name = input("Enter your name:")
+#print(f"Hello {name}")
+first_number = None
+while first_number is None:     
+    try:
+         first_number = int(input("Enter the first number: "))
+    except ValueError:
+       print("That's not a valid number.")
+
+second_number = None
+while second_number is None:     
+    try:
+         second_number = int(input("Enter the second number: "))
+    except ValueError:
+       print("That's not a valid number.")
+       
+        #if second_number == 0
+            #print("That's not a valid number.")
+
+#print(f"Do you want a {fav2} {fav1} with {fav3} legs?")
